09fd9405a48fd5088386d230a3cbbb32b4929619
Rémi Ait-Younes <remi@ait-younes.fr>
2023-12-17 15:45:55 +0100

    web: bind on 0.0.0.0

--- services/web/src/main.rs
+++ services/web/src/main.rs
@@ -58,7 +58,7 @@ async fn main() -> anyhow::Result<()> {
     HttpServer::new(move || {
         App::new().service(greet).app_data(web::Data::new(Data(conn.clone(), std::env::var("ROBLOSECURITY").ok())))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await?;
     Ok(())
