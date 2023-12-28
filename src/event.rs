3408d2ddb4c947d7bdf8b69f3689b09b450cb002
Rémi Ait-Younes <remi@ait-younes.fr>
2023-12-28 16:07:53 +0100

    removed redundant if

--- src/event.rs
+++ src/event.rs
@@ -22,45 +22,43 @@ pub async fn event_handler(
             let merged_interaction = MergedInteraction::SerenityMessageComponentInteraction(interaction.clone());
             match interaction.data.custom_id.as_str() {
                 "link" => {
-                    if interaction.data.custom_id == "link" {
-                        interaction.create_interaction_response(&ctx.http, |r| {
-                            r.kind(serenity::InteractionResponseType::Modal)
-                            .interaction_response_data(|d| {
-                                d.content("Linking process")
-                                .custom_id("roblox_link")
-                                .title("What is your roblox username ?")
-                                .components(|c| {
-                                    c.create_action_row(|r| r.create_input_text(|i| {
-                                        i.custom_id("roblox_username")
-                                        .placeholder("Roblox username")
-                                        .label("Roblox username")
-                                        .style(serenity::InputTextStyle::Short)
-                                        .max_length(20)
-                                        .required(true)
-                                    }))
-                                })
+                    interaction.create_interaction_response(&ctx.http, |r| {
+                        r.kind(serenity::InteractionResponseType::Modal)
+                        .interaction_response_data(|d| {
+                            d.content("Linking process")
+                            .custom_id("roblox_link")
+                            .title("What is your roblox username ?")
+                            .components(|c| {
+                                c.create_action_row(|r| r.create_input_text(|i| {
+                                    i.custom_id("roblox_username")
+                                    .placeholder("Roblox username")
+                                    .label("Roblox username")
+                                    .style(serenity::InputTextStyle::Short)
+                                    .max_length(20)
+                                    .required(true)
+                                }))
                             })
-                        }).await?;
-                        let modal_response = &interaction.get_interaction_response(&ctx.http).await?;
-                        let interaction = match modal_response
-                            .await_modal_interaction(ctx)
-                            .timeout(std::time::Duration::from_secs(60 * 3))
-                            .await
-                        {
-                            Some(x) => x,
-                            None => {
-                                return Ok(());
-                            }
-                        };
-                        let merged_interaction = MergedInteraction::SerenityModalSubmitInteraction(interaction.as_ref().clone());
-                        let username: &ActionRowComponent = &interaction.data.components[0].components[0];
-                        let username = match username {
-                            ActionRowComponent::InputText(txt) => &txt.value,
-                            _ => return Ok(()),
-                        };
-                        link_backend(interaction.user.id.0 as i64, data, &merged_interaction, &ctx.http, username).await?;
+                        })
+                    }).await?;
+                    let modal_response = &interaction.get_interaction_response(&ctx.http).await?;
+                    let interaction = match modal_response
+                        .await_modal_interaction(ctx)
+                        .timeout(std::time::Duration::from_secs(60 * 3))
+                        .await
+                    {
+                        Some(x) => x,
+                        None => {
+                            return Ok(());
+                        }
+                    };
+                    let merged_interaction = MergedInteraction::SerenityModalSubmitInteraction(interaction.as_ref().clone());
+                    let username: &ActionRowComponent = &interaction.data.components[0].components[0];
+                    let username = match username {
+                        ActionRowComponent::InputText(txt) => &txt.value,
+                        _ => return Ok(()),
+                    };
+                    link_backend(interaction.user.id.0 as i64, data, &merged_interaction, &ctx.http, username).await?;
         
-                    }
                 },
                 "complete" => {
                     complete_backend(&interaction.user.id, data, &merged_interaction, &ctx.http).await?;
